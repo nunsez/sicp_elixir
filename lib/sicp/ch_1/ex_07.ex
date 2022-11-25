b3505e884d449343170e0effab702955c4c8dc40
Alexander Mandrikov <mandrikov@pm.me>
2022-11-25 17:50:05 +0700

    add ch1ex07

---
+++ lib/sicp/ch_1/ex_07.ex
@@ -0,0 +1,40 @@
+defmodule SICP.Ch1.Ex07 do
+  @moduledoc """
+  Task:
+
+  The good-enough? test used in computing square roots will not be very effective
+  for finding the square roots of very small numbers. Also, in real computers,
+  arithmetic operations are almost always performed with limited precision. This
+  makes our test inadequate for very large numbers. Explain these statements,
+  with examples showing how the test fails for small and large numbers. An
+  alternative strategy for implementing good-enough? is to watch how guess
+  changes from one iteration to the next and to stop when the change is a very
+  small fraction of the guess. Design a square-root procedure that uses this kind
+  of end test. Does this work better for small and large numbers?
+  """
+
+  @spec square_root(number()) :: float()
+  def square_root(x), do: sqrt_iter(1.0, x)
+
+  @spec sqrt_iter(float(), number()) :: float()
+  defp sqrt_iter(guess, x) do
+    improved = improve(guess, x)
+
+    if good_enough?(guess, improved) do
+      guess
+    else
+      sqrt_iter(improved, x)
+    end
+  end
+
+  @delta 1.0e-7
+
+  @spec good_enough?(number(), number()) :: boolean()
+  defp good_enough?(guess, x), do: abs(x - guess) < @delta
+
+  @spec improve(float(), number()) :: float()
+  defp improve(guess, x), do: average(guess, x / guess)
+
+  @spec average(float(), number()) :: float()
+  defp average(x, y), do: (x + y) / 2
+end
---
+++ test/ch_1/ex_07_test.exs
@@ -0,0 +1,30 @@
+defmodule SICP.Ch1.Ex07Test do
+  use ExUnit.Case
+  doctest SICP.Ch1.Ex07
+
+  import SICP.Ch1.Ex07, only: [square_root: 1]
+
+  @tag task: 107
+  test "small" do
+    result = square_root(0.04) * 1_000
+    assert round(result) === 200
+  end
+
+  @tag task: 107
+  test "decimal" do
+    result = square_root(4.0) * 1_000
+    assert round(result) === 2_000
+  end
+
+  @tag task: 107
+  test "medium" do
+    result = square_root(100) * 1_000
+    assert round(result) === 10_000
+  end
+
+  @tag task: 107
+  test "large" do
+    result = square_root(1_000_000.0) * 1_000
+    assert round(result) === 1_000_000
+  end
+end
