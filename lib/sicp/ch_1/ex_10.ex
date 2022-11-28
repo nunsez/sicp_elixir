1e7dec65b2dbe4a8de7c64cfa4e56c67e4c35959
Alexander Mandrikov <mandrikov@pm.me>
2022-11-28 21:36:50 +0700

    use doctests for ch1ex10

--- lib/sicp/ch_1/ex_10.ex
+++ lib/sicp/ch_1/ex_10.ex
@@ -1,6 +1,7 @@
 defmodule SICP.Ch1.Ex10 do
   @moduledoc """
   Task:
+
   The following procedure computes a mathematical function called
   Ackermann's function.
 
@@ -41,46 +42,49 @@ defmodule SICP.Ch1.Ex10 do
 
   Answer:
 
-  ```scheme
-  (A 1 10)
+  iex> SICP.Ch1.Ex10.ackermann_function(1, 10)
   1_024
 
-  (A 2 4)
-  65_535
+  iex> SICP.Ch1.Ex10.ackermann_function(2, 4)
+  65_536
 
-  (A 3 3) ;; equals (A 2 4)
-  65_535
+  iex> SICP.Ch1.Ex10.ackermann_function(3, 3)
+  65_536
+  """
+
+  @doc """
+  ```scheme
+  (define (A x y)
+    (cond ((= y 0) 0)
+          ((= x 0) (* 2 y))
+          ((= y 1) 2)
+          (else (A (- x 1)
+                   (A x (- y 1))))))
   ```
   """
+  @spec ackermann_function(number(), number()) :: number()
+  def ackermann_function(_, 0), do: 0
+  def ackermann_function(0, y), do: 2 * y
+  def ackermann_function(_, 1), do: 2
+
+  def ackermann_function(x, y) do
+    ackermann_function(x - 1, ackermann_function(x, y - 1))
+  end
 
   # Procedures
 
-  @spec ackermann_function(number(), number()) :: number()
-  def ackermann_function(x, y) do
-    cond do
-      y == 0 -> 0
-      x == 0 -> 2 * y
-      y == 1 -> 2
-      true -> ackermann_function(x - 1, ackermann_function(x, y - 1))
-    end
-  end
-
   @doc """
-  Procedure `f`
-
   ```scheme
   (define (f n) (A 0 n))
-  (define (f n) (* 2 n))
   ```
   """
   @spec f(number()) :: number()
   def f(n), do: ackermann_function(0, n)
 
   @doc """
-  Procedure `g`
-
   ```scheme
   (define (g n) (A 1 n))
+
   (A 0 (A 1 (- n 1)))
   (* 2 (A 1 (- n 1)))
   (* 2 (* 2 (A 1 (- n 2)))) ; ect.
@@ -90,8 +94,6 @@ defmodule SICP.Ch1.Ex10 do
   def g(n), do: ackermann_function(1, n)
 
   @doc """
-  Procedure `h`
-
   https://en.wikipedia.org/wiki/Tetration
 
   ```scheme
@@ -102,10 +104,6 @@ defmodule SICP.Ch1.Ex10 do
   def h(n), do: ackermann_function(2, n)
 
   @doc """
-  Procedure `k`
-
-  Need not be explained
-
   ```scheme
   (define (k n) (* 5 n n))
   ```
@@ -113,16 +111,69 @@ defmodule SICP.Ch1.Ex10 do
   @spec k(number()) :: number()
   def k(n), do: 5 * n * n
 
-  @spec my_f(number()) :: number()
-  def my_f(n), do: 2 * n
+  # Concise mathematical definitions
 
-  @spec my_g(number()) :: number()
-  def my_g(0), do: 0
-  def my_g(1), do: 2
-  def my_g(n), do: my_f(my_g(n - 1))
+  @doc """
+  ```
+  f = 2n
+  ```
 
-  @spec my_h(number()) :: number()
-  def my_h(0), do: 0
-  def my_h(1), do: 2
-  def my_h(n), do: my_g(my_h(n - 1))
+  iex> SICP.Ch1.Ex10.f_explained(2) === SICP.Ch1.Ex10.f(2)
+  true
+
+  iex> SICP.Ch1.Ex10.f_explained(3) === SICP.Ch1.Ex10.f(3)
+  true
+
+  iex> SICP.Ch1.Ex10.f_explained(4) === SICP.Ch1.Ex10.f(4)
+  """
+  @spec f_explained(number()) :: number()
+  def f_explained(n) when n >= 0, do: 2 * n
+
+  @doc """
+  ```
+  g = 0, n = 0
+  g = 2, n = 1
+  g = f(g(n - 1)), n > 1
+  ```
+
+  iex> SICP.Ch1.Ex10.g_explained(3) === SICP.Ch1.Ex10.g(3)
+  true
+
+  iex> SICP.Ch1.Ex10.g_explained(4) === SICP.Ch1.Ex10.g(4)
+  true
+  """
+  @spec g_explained(number()) :: number()
+  def g_explained(0), do: 0
+  def g_explained(1), do: 2
+  def g_explained(n) when n > 1, do: f_explained(g_explained(n - 1))
+
+  @doc """
+  ```
+  h = 0, n = 0
+  h = 2, n = 1
+  h = g(h(n - 1)), n > 1
+  ```
+
+  iex> SICP.Ch1.Ex10.h_explained(3) === SICP.Ch1.Ex10.h(3)
+  true
+
+  iex> SICP.Ch1.Ex10.h_explained(4) === SICP.Ch1.Ex10.h(4)
+  true
+  """
+  @spec h_explained(number()) :: number()
+  def h_explained(0), do: 0
+  def h_explained(1), do: 2
+  def h_explained(n) when n > 1, do: g_explained(h_explained(n - 1))
+
+  @doc """
+  Need not be explained
+
+  iex> SICP.Ch1.Ex10.k_explained(3) === SICP.Ch1.Ex10.k(3)
+  true
+
+  iex> SICP.Ch1.Ex10.k_explained(4) === SICP.Ch1.Ex10.k(4)
+  true
+  """
+  @spec k_explained(number()) :: number()
+  def k_explained(n), do: 5 * n * n
 end
--- test/ch_1/ex_10_test.exs
+++ test/ch_1/ex_10_test.exs
@@ -1,27 +1,4 @@
 defmodule SICP.Ch1.Ex10Test do
   use ExUnit.Case
   doctest SICP.Ch1.Ex10
-
-  import SICP.Ch1.Ex10
-
-  @tag task: 110
-  test "my_f" do
-    Enum.each([0, 1, 2, 3], fn(n) ->
-      assert f(n) === my_f(n)
-    end)
-  end
-
-  @tag task: 110
-  test "my_g" do
-    Enum.each([0, 1, 2, 3], fn(n) ->
-      assert g(n) === my_g(n)
-    end)
-  end
-
-  @tag task: 110
-  test "my_h" do
-    Enum.each([0, 1, 2, 3], fn(n) ->
-      assert h(n) === my_h(n)
-    end)
-  end
 end
