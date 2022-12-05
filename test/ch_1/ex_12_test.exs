2a5a843ae2bf993a381bebeb8c9b0197545bd36a
Alexander Mandrikov <mandrikov@pm.me>
2022-12-05 14:51:25 +0700

    add ch1ex12

---
+++ test/ch_1/ex_12_test.exs
@@ -0,0 +1,14 @@
+defmodule SICP.Ch1.Ex11Test do
+  use ExUnit.Case
+  doctest SICP.Ch1.Ex12
+
+  import SICP.Ch1.Ex12, only: [solution: 2]
+
+  test "pascal's triangle" do
+    assert solution(1, 1) === 1
+    assert solution(3, 2) === 2
+    assert solution(4, 3) === 3
+    assert solution(5, 2) === 4
+    assert solution(5, 3) === 6
+  end
+end
