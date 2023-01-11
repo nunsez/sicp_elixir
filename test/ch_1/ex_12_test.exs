0be8911f1f254df3e254e6bf362dba2dfdc5d993
Alexander Mandrikov <mandrikov@pm.me>
2023-01-11 14:28:42 +0700

    add tag to ch_1/ex_12_test.exs

--- test/ch_1/ex_12_test.exs
+++ test/ch_1/ex_12_test.exs
@@ -4,6 +4,7 @@ defmodule SICP.Ch1.Ex12Test do
 
   import SICP.Ch1.Ex12, only: [solution: 2]
 
+  @tag task: 112
   test "pascal's triangle" do
     assert solution(1, 1) === 1
     assert solution(3, 2) === 2
