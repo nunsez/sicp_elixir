fd28dac1ef166dbc81eabd0f27e6885b1846b4c0
Alexander Mandrikov <mandrikov@pm.me>
2023-01-23 16:56:20 +0700

    add ch1ex21

---
+++ lib/sicp/ch_1/ex_21.ex
@@ -0,0 +1,37 @@
+defmodule SICP.Ch1.Ex21 do
+  @moduledoc """
+  Task:
+
+  Use the smallest-divisor procedure to find the smallest divisor of each of the
+  following numbers: `199`, `1999`, `19999`.
+
+  Answer:
+
+      iex> SICP.Ch1.Ex21.smallest_divisor(199)
+      199
+
+      iex> SICP.Ch1.Ex21.smallest_divisor(1_999)
+      1999
+
+      iex> SICP.Ch1.Ex21.smallest_divisor(19_999)
+      7
+  """
+
+  @spec smallest_divisor(integer()) :: integer()
+  def smallest_divisor(n), do: find_divisor(n, 2)
+
+  @spec find_divisor(integer(), integer()) :: pos_integer()
+  defp find_divisor(n, test_divisor) do
+    cond do
+      square(test_divisor) > n -> n
+      divides?(test_divisor, n) -> test_divisor
+      true -> find_divisor(n, test_divisor + 1)
+    end
+  end
+
+  @spec square(non_neg_integer()) :: float()
+  defp square(n) when n >= 0, do: :math.sqrt(n)
+
+  @spec divides?(integer(), integer()) :: boolean()
+  defp divides?(a, b), do: rem(b, a) == 0
+end
---
+++ test/ch_1/ex_21_test.exs
@@ -0,0 +1,4 @@
+defmodule SICP.Ch1.Ex21Test do
+  use ExUnit.Case
+  doctest SICP.Ch1.Ex21
+end
