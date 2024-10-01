511c2bca3b9541a33381fa035db75ae831c17bfd
Alexander Mandrikov <mandrikov@pm.me>
2024-10-01 10:18:15 +0700

    add ch1ex30

---
+++ test/ch_1/ex_30_test.exs
@@ -0,0 +1,25 @@
+defmodule SICP.Ch1.Ex30Test do
+  use ExUnit.Case
+  doctest SICP.Ch1.Ex30
+
+  alias SICP.Ch1.Ex29
+  alias SICP.Ch1.Ex30
+
+  import SICP.Common, only: [cube: 1, inc: 1]
+
+  @tag task: 130
+  test "equality" do
+    assert Ex29.sum(&cube/1, 1, &inc/1, 10) == Ex30.sum(&cube/1, 1, &inc/1, 10)
+  end
+
+  @tag :benchmark
+  @tag task: 130
+  test "benchmark" do
+    Benchee.run(
+      %{
+        "Ex29 sum" => fn -> Ex29.sum(&cube/1, 1, &inc/1, 50) end,
+        "Ex30 iter sum" => fn -> Ex30.sum(&cube/1, 1, &inc/1, 50) end
+      }
+    )
+  end
+end
